c2ddacb630bf8c9332dba20625e99898ee955f57
Joshua Johnston <36349396+JJohnston89@users.noreply.github.com>
2018-02-26 22:06:22 -0500

    Create my.h

---
+++ my.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+int* getData(char* file_name, int* size);
+void swap(int* array1, int one, int two);
+void even_odd_sort(int* array1, int array_size);
+void display(int* array1, int size);
+int main ( int argc, char *argv[]);
